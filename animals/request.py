ec1aa3dda9669ea4f841b3854ce12c60d10445da
Travis Hall <t.hall89d@gmail.com>
2021-10-11 17:02:49 -0400

    made create_animal fn and updated do_POST fn

--- animals/request.py
+++ animals/request.py
@@ -35,3 +35,20 @@ def get_single_animal(id):
             requested_animal = animal
 
     return requested_animal
+
+
+def create_animal(animal):
+    # Get the id value of the last animal in the list
+    max_id = ANIMALS[-1]["id"]
+
+    # Add 1 to last index id
+    new_id = max_id + 1
+
+    # add an `id` property to the animal dictionary
+    animal["id"] = new_id
+
+    # Add the animal dictionary to the list
+    ANIMALS.append(animal)
+
+    # Return the dictionary with `id` property added
+    return animal
