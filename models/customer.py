1fccfe186e27f66d5462b0082a27cbb255259641
Travis Hall <t.hall89d@gmail.com>
2021-10-13 16:44:16 -0400

    models

---
+++ models/customer.py
@@ -0,0 +1,9 @@
+class Customer():
+    """defines what properties will be on an object representation of a customer
+    """
+
+    def __init__(self, id, name, email, password):
+        self.id = id
+        self.name = name
+        self.email = email
+        self.password = password
---
+++ models/location.py
@@ -0,0 +1,7 @@
+class Location():
+    """defines what properties will be on an object representation of a location
+    """
+
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
